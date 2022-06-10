834bb1673e5c9818764ff5db2b684d8271afa734
mludwig99 <michalludwig99@gmail.com>
2022-06-10 19:51:45 +0200

    37-poprawione

--- script/main.js
+++ script/main.js
@@ -1,9 +1,19 @@
 let num1 = 2;
 let num2 = 5;
 
-function caunt(a, b) {
-    let result = num1 + num2;
-    return result
+function caunt(a,b) {
+    if ( a > b) {
+        console.log(a);
+    }
+    else {
+        console.log(b);
+    }
+    
 }
-
-console.log(caunt());
+console.log(caunt(num1,num2))
+/*
+function caunt2(a, b) {
+    let countFinal = Math.max(a, b);
+    return countFinal;
+}
+console.log(caunt2(num1, num2))*/
