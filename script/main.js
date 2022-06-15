82a757f55ff3ee5258984520b8ba861a3b8e59d8
mludwig99 <michalludwig99@gmail.com>
2022-06-15 11:03:03 +0200

    37-poprawione

--- script/main.js
+++ script/main.js
@@ -1,19 +1,21 @@
 let num1 = 2;
 let num2 = 5;
 
-function caunt(a,b) {
-    if ( a > b) {
-        console.log(a);
+function caunt(a, b) {
+    if (a > b) {
+        return a;
+    } else if (a === b) {
+
+        return 'Są równe';
+    } else {
+        return b +' jest większa od ' + a;
     }
-    else {
-        console.log(b);
-    }
-    
+
 }
-console.log(caunt(num1,num2))
-/*
-function caunt2(a, b) {
-    let countFinal = Math.max(a, b);
-    return countFinal;
-}
-console.log(caunt2(num1, num2))*/
+console.log(caunt(num1, num2))
+
+// function caunt2(a, b) {
+//     let countFinal = Math.max(a, b);
+//     return countFinal;
+// }
+// console.log(caunt2(num1, num2))
